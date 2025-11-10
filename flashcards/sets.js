34c3154df5e1bbfe7f1d87e333ca2aecb42d6cf9
redox7500 <redox7500@gmail.com>
2025-11-09 20:10:42 -0800

    Adds Ecce Romani Chapter 35 vocabulary flashcard set

--- flashcards/sets.js
+++ flashcards/sets.js
@@ -1,9 +1,26 @@
 const sets = {
-    "test":{
-        "a":"1",
-        "b":"2",
-        "c":"3",
-        "d":"4",
-        "e":"5"
+    "Ecce Romani Chapter 35":{
+        "collis":"collis, gen. pl. collium, m. hill",
+        "deus":"-ī, nom. pl. dī, m. god",
+        "vulnus":"vulneris, n. wound",
+        "fūstis":"fūstis, gen. pl. fūstium, m. club/cudgel",
+        "timor":"timōris, m. fear",
+        "tergum":"-ī, n. back",
+        "sēcūrus":"-a, -um, carefree/free/unconcerned",
+        "affectus":"-a, -um, overcome/affected",
+        "prōnus":"-a, -um, face down",
+        "summus":"-a, -um, greatest/very great/highest",
+        "gravis":"-is, -e, heavy/serious",
+        "dīligēns":"dīligentis, diligent, painstaking/thorough",
+        "certus":"-a, -um, certain",
+        "rēctus":"-a, -um, right/proper/straight",
+        "ligō":"-āre, -āvī, -ātus, to bind up",
+        "crēdō":"crēdere, crēdidī, crēditus, (+dat) to believe/trust",
+        "percutiō":"percutere, percussī, percussus, to strike",
+        "corripiō":"corripere, corripuī, correptus, to seize/grab",
+        "adimō":"adimere, adēmī, adēmptus, to take away (from)",
+        "quam":"than",
+        "quam + superlative":"as...as possible",
+        "cōnsecutī sunt":"they overtook"
     }
 };
